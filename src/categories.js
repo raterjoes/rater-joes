ad2f1cf6ba232766ad7f4dbcea41e6a404ba1097
Victoria Alkin <raterjoes.contact@gmail.com>
2025-04-02 20:23:40 -0400

    New categories added

--- src/categories.js
+++ src/categories.js
@@ -1,3 +1,3 @@
-const categories = ["Produce", "Frozen Foods", "Desserts", "Seasonings", "Snacks", "Ready-to-eat", "Bakery", "Dairy", "Cheeses", "Sauces, Dips, and Spreads", "Other"];
+const categories = ["Produce", "Frozen Foods", "Desserts", "Seasonings", "Snacks", "Ready-to-eat", "Bakery", "Dairy", "Cheeses", "Sauces, Dips, and Spreads", "Drinks", "Other"];
 
 export default categories;
